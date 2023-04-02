98bdb4c50982e9c836fcbeb4e48f34232cdd4fa5
Anastasia Dorokhina <sota54@mail.ru>
2023-04-02 19:11:20 +0400

    second commit: print only one fibonacci number, whose number was entered, start with 1, 1...

--- hometask.cpp
+++ hometask.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int fib(int n){
+	if (n == 1 || n == 2) return 1;
+	else return fib(n - 1) + fib(n - 2);
+}
+
 int main(){
-    cout << "Hello, World!" << endl;
-    return 0;
+	int n;
+	cout << "Hello, World!" << endl;
+	cout << "please, enter number n\n";
+	cout << "n = ";
+	cin >> n;
+	cout << fib (n);
+	return 0;
 }
