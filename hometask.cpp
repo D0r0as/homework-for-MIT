9fd05b23e0a34816b4c80835fb145dbcc94bea36
Anastasia Dorokhina <sota54@mail.ru>
2023-04-02 21:49:55 +0400

    fourth commit: print all fibonacci numbers up to n

--- hometask.cpp
+++ hometask.cpp
@@ -2,9 +2,16 @@
 using namespace std;
 
 int fib(int n){
+	int k = 2, f0 = 0, f1 = 1, f2;
 	if (n == 1) return 0;
 	else if (n == 2) return 1;
-	else return fib(n - 1) + fib(n - 2);
+	while (k < n){
+		f2 = f1 + f0;
+		f0 = f1;
+		f1 = f2;
+		k++;
+	}
+	return f2;
 }
 
 int main(){
@@ -13,6 +20,7 @@ int main(){
 	cout << "please, enter number n\n";
 	cout << "n = ";
 	cin >> n;
-	cout << fib (n);
+	for (int i = 1; i <= n; i++)
+		cout << fib (i) << endl;
 	return 0;
 }
